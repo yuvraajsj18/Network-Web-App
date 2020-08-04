dafdcf13ce6a5484259db2ac2885c1fc6f0c2a38
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2020-08-04 15:53:09 +0530

    connected postgres database 1111

--- project4/settings.py
+++ project4/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["network-web-app-0.herokuapp.com", 'localhost']
 
