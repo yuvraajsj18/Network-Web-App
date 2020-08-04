f667e5e98c195530efd21b100b4dfc20b375f1c3
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2020-08-04 15:19:27 +0530

    add database conn

--- project4/settings.py
+++ project4/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'project4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'NETWORK_DATABASE',
+        'USER': 'postgres',
+        'PASSWORD': 'mypostgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
